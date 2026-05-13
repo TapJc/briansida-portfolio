d2e89ac9f8c27cbb4b498a993a0fce0273c63c07
Brian S <brian209222@gmail.com>
2026-05-13 16:46:16 -0500

    Updated About component with personal details and scrollbar visibility

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -18,7 +18,37 @@ function About() {
         </div>
       </div>
       <div className={styles.details}>
-
+        <div>
+          <p style={{marginBottom: "15px"}}>Hi, I'm Brian, a CS graduate and devoloper. I've...</p>
+          <ul>
+            <li>collaborated with developers and UX designers to create user-focused sites</li>
+            <li>developed responsive UIs and RESTful APIs in Agile team environments</li>
+            <li>created projects ranging from e-commerce platforms to interactive games</li>
+          </ul>
+        </div>
+        <div>
+          <span style={{fontFamily:"var(--font-heading)", fontSize:"1.5rem", fontWeight:"bold"}}>EDUCATION</span>
+          <div style={{borderLeft: "3px solid var(--color-border)", marginTop:"8px"}}>
+            <span style={{paddingLeft:"15px"}}>Bachelor of Science in Computer Science</span>
+            <br/>
+            <span style={{paddingLeft:"15px", color:"var(--color-text-muted)", fontSize:"0.9rem"}}>(Graduated Cum Laude 2024)</span>
+          </div>
+        </div>
+        <div>
+          <span style={{fontFamily:"var(--font-heading)", fontSize:"1.5rem", fontWeight:"bold"}}>INTERESTS</span>
+          <ul style={{marginTop:"15px"}}>
+            <li>role playing games</li>
+            <li>psychological movies/shows</li>
+            <li>reading comics and novels</li>
+            <li>hiking or riding my bike</li>
+          </ul>
+        </div>
+        <div>
+          <span style={{fontFamily:"var(--font-heading)", fontSize:"1.5rem", fontWeight:"bold"}}>LANGUAGE PROFICIENCY</span>
+          <div style={{borderLeft: "3px solid var(--color-border)", marginTop: "8px"}}>
+            <span style={{paddingLeft:"15px"}}>I have native fluency in English and can speak Spanish</span>
+          </div>
+        </div>
       </div>
     </div>
   );
