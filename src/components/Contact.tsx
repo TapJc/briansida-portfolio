0a27a9dd44990a8af425a096f49829e25f31fa78
Brian S <brian209222@gmail.com>
2026-05-14 20:05:27 -0500

    Updated Contact component with email functionality and styling updates

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,16 +1,16 @@
 import styles from "../styles/Contact.module.css";
-import snoopyMail from "../snoopyMail.png";
+import mail from "../assets/images/mail.png";
 
 function Contact() {
   return (
     <div className={styles.contact}>
-      <span className={styles.title}>Email</span>
-      <p className={styles.description}>The easiest way to contact me is through email. 
-        Direct any requests or inquiries at my email: <span style={{color:"var(--color-accent)", fontWeight:"bold"}}>brian209222@gmail.com</span>
+      <span className={styles.contactTitle}>Email</span>
+      <p className={styles.contactDescription}>The easiest way to contact me is through email. 
+        Direct any requests or inquiries at my email: <span className={styles.emailHighlight} title="Copy to Clipboard">brian209222@gmail.com</span>
       </p>
-      <img className={styles.mailImg} src={snoopyMail} alt="snoopyMail"></img>
-      <p className={styles.description}>or press the button below to open your mail app</p>
-      <button className={styles.mailBtn} style={{color:"black", fontWeight:"bold"}}>Send Email</button>
+      <img className={styles.contactImage} src={mail} alt="mail"></img>
+      <p className={styles.contactDescription}>or press the button below to open your mail app</p>
+      <button className={styles.contactBtn} onClick={() => {window.location.href = "mailto:brian209222@gmail.com"}}>Send Email</button>
     </div>
   );
 }
