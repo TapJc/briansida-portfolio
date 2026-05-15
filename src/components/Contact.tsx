e403ef5f6ff9c05a7db674c5dd985f3eb2e7e426
Brian S <brian209222@gmail.com>
2026-05-15 15:20:15 -0500

    Updated Contact component with tooltip styling

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,24 @@
 import styles from "../styles/Contact.module.css";
 import mail from "../assets/images/mail.png";
+import { useState } from "react";
 
 function Contact() {
+  const [copied, setCopied] = useState(false);
+
+  function emailCopied() {
+    navigator.clipboard.writeText("brian209222@gmail.com");
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false)
+    }, 6000); // Resets every two seconds
+  }
+
   return (
     <div className={styles.contact}>
       <span className={styles.contactTitle}>Email</span>
       <p className={styles.contactDescription}>The easiest way to contact me is through email. 
-        Direct any requests or inquiries at my email: <span className={styles.emailHighlight} title="Copy to Clipboard">brian209222@gmail.com</span>
+        Direct any requests or inquiries at my email: <span className={copied ? styles.emailCopied : styles.emailHighlight} onClick={emailCopied}>brian209222@gmail.com</span>
       </p>
       <img className={styles.contactImage} src={mail} alt="mail"></img>
       <p className={styles.contactDescription}>or press the button below to open your mail app</p>
