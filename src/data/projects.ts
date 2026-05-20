80d661f314d8c648c51e10a6ab912e5504166d04
Brian S <brian209222@gmail.com>
2026-05-20 18:26:16 -0500

    Add project images and updated project list in Work panel

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,23 @@
 // Static list of projects displayed in the Work panel
 export const projects = [
-  {title: "a", img: "a", description: "a", link: "a"},
-  {title: "b", img: "b", description: "b", link: "b"},
-  {title: "c", img: "c", description: "c", link: "c"}
+  {
+    title: "\"Amazon\"", 
+    img: "../assets/images/projects/amazon.png", 
+    description: "a", 
+    link: "https://github.com/TapJc/ecommerce-frontend"
+  },
+  {
+    title: "PeoriaFresh", 
+    img: "../assets/images/projects/peoriaFresh.png", 
+    description: "b", 
+    link: "https://peoriafresh.org/"
+  },
+  {
+    title: "HealthScape", 
+    img: "./assets/images/projects/healthScape.png", 
+    description: "c", 
+    link: "https://kmann725.itch.io/healthscape"
+  }
 ];
 
 // Static list of frameworks & technologies displayed in the experience section in Work panel
