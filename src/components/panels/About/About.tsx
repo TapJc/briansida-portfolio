1d41f759d52481face7bbc78e671cedfa5eea5b1
Brian S <brian209222@gmail.com>
2026-06-21 15:19:56 -0500

    Updated theme color variables

--- src/components/panels/About/About.tsx
+++ src/components/panels/About/About.tsx
@@ -1,5 +1,5 @@
 import styles from "./About.module.css"
-import profile from "../../../assets/images/profile.jpg";
+import profile from "../../../assets/images/profile.png";
 
 function About() {
   return (
